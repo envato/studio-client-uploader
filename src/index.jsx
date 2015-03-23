2345230818dd93f2c456220462ed6f3133fe5c90
Shervin Aflatooni <shervinaflatooni@gmail.com>
2015-03-23 18:05:29 +1100

    Import asset service nom package

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Dropzone from './vendor/dropzone';
+import StudioAssetService from 'studio-asset-service-client-js';
 
 var Uploader = React.createClass({
   componentDidMount: function() {
-    window.z = Dropzone;
+    var client = new StudioAssetService('https://studio-asset-api.envato.com');
+    client.getAsset("e030de73-f951-48c9-a157-3d41e13b72f1", function(err, assets) {
+      console.log('asset: ', err, assets.getVariant(":original"));
+    });
   },
    render: function() {
     return (
-      <div>Uploader lol hello</div>
+      <div className="asset-uploader">
+        HELLO
+      </div>
     );
   }
 });
