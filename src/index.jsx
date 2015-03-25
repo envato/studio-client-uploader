455c99606d720a30d38a8430b3845f47e0810514
Shervin Aflatooni <shervinaflatooni@gmail.com>
2015-03-25 21:26:13 +1100

    allow asset type

--- src/index.jsx
+++ src/index.jsx
@@ -36,7 +36,7 @@ var Uploader = React.createClass({
     var self = this;
 
     file.acceptDimensions = function() {
-      self.assetServiceClient.newAsset(function(err, data) {
+      self.assetServiceClient.newAsset(self.props.assetType, function(err, data) {
         file.signature = data.transloadit.signature;
         file.params    = data.transloadit.params;
         file.assetId   = data.asset.id;
