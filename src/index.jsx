b1e47dd9627cc2491175fc03d8b3a2257a8f6e6d
Shervin Aflatooni <shervinaflatooni@gmail.com>
2015-03-26 10:45:33 +1100

    Forgot call!, actually clear here

--- src/index.jsx
+++ src/index.jsx
@@ -87,6 +87,7 @@ var Uploader = React.createClass({
     });
   },
   componentWillUnmount: function() {
+    UploadStore.clear();
     UploadStore.removeListener('change', this._onChange);
     this.dropzone.destroy();
     this.dropzone = null;
